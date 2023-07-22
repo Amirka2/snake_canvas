c14ed04e9e3d85b1a8df9e12b3cf3324cab964c4
amir_mac <far20022002@mail.ru>
2023-07-22 23:20:24 +0500

    differentiated controller and snake model

--- script.js
+++ script.js
@@ -28,54 +28,8 @@ let rand = function (min, max) {
 class Snake {
     constructor() {
         this.coords = [{x: 0, y: 0}];
-    }
-
-    move = function (dir) {
-        let action = {x: 0, y: 0};
-        if (dir === directions.RIGHT) {
-            action.x = this.coords[0].x + s;
-            action.y = this.coords[0].y;
-        } else if (dir === directions.BOTTOM) {
-            action.x = this.coords[0].x;
-            action.y = this.coords[0].y + s;
-        } else if (dir === directions.LEFT) {
-            action.x = this.coords[0].x - s;
-            action.y = this.coords[0].y;
-        } else if (dir === directions.TOP) {
-            action.x = this.coords[0].x;
-            action.y = this.coords[0].y - s;
-        }
-
-        this.coords.unshift(action);
-        return this.coords.pop();
-    }
-
-    increase(dir) {
-        let tail = this.move(dir);
-        this.coords.unshift(tail);
-    }
-
-    iterate(dir, apple) {
-        if (this.checkDeath())
-            return false;
-        if (!this.checkApple(apple))
-            this.move(dir);
-        else
-            this.increase();
-        return true;
-    }
-
-    checkApple(apple) {
-        return (this.coords[0].x === apple.x && this.coords[0].y === apple.y);
-    }
-
-    checkDeath() {
-        this.coords.forEach((coord) => {
-            if (this.coords[0].x === coord.x && this.coords[0].y === coord.y) {
-                return true;
-            }
-        })
-        return false;
+        this.head = this.coords[0];
+        this.tail = this.coords[0];
     }
 }
 
@@ -109,6 +63,49 @@ class Game {
         this.height = height;
     }
 
+    checkApple(snake, apple) {
+        return (snake.coords[0].x === apple.x && snake.coords[0].y === apple.y);
+    }
+
+    checkDeath(snake) {
+        snake.coords.forEach((coord) => {
+            if (snake.coords[0].x === coord.x && snake.coords[0].y === coord.y) {
+                return true;
+            }
+        })
+        return false;
+    }
+
+    moveSnake = function (snake, dir) {
+        let action = {x: 0, y: 0};
+        if (dir === directions.RIGHT) {
+            action.x = snake.coords[0].x + s;
+            action.y = snake.coords[0].y;
+        } else if (dir === directions.BOTTOM) {
+            action.x = snake.coords[0].x;
+            action.y = snake.coords[0].y + s;
+        } else if (dir === directions.LEFT) {
+            action.x = snake.coords[0].x - s;
+            action.y = snake.coords[0].y;
+        } else if (dir === directions.TOP) {
+            action.x = snake.coords[0].x;
+            action.y = snake.coords[0].y - s;
+        }
+
+        snake.head = action;
+        snake.coords.unshift(snake.head);
+        snake.tail = snake.coords.pop();
+        return snake.tail;
+    }
+
+    increaseSnake (snake, dir) {
+        if (snake.coords.length > 5) {
+            console.log(snake.coords);
+        }
+        this.moveSnake(snake, dir);
+        snake.coords.push(snake.tail);
+    }
+
     start() {
         let gameField = document.getElementById('snake_game');
         gameField.style.border = '1px solid black';
@@ -121,13 +118,22 @@ class Game {
         let s = 30;
 
         let g = gameField.getContext('2d');
-        addEventListener('keydown', e => handleClick(e))
+        addEventListener('keydown', e => {
+            handleClick(e);
+            console.log(snake);
+        })
 
         let drawer = new Drawer(this.width, this.height);
 
         setInterval(() => {
-            if (!snake.iterate(dir, apple))
+            if (this.checkDeath(snake))
                 this.end();
+            if (!this.checkApple(snake, apple))
+                this.moveSnake(snake, dir);
+            else {
+                this.increaseSnake(snake, dir);
+                apple = new Apple();
+            }
             drawer.draw(g, snake.coords, apple);
         }, timeout);
     }
@@ -137,5 +143,5 @@ class Game {
     }
 }
 
-game = new Game(250, 450);
+game = new Game(450, 450);
 game.start();
